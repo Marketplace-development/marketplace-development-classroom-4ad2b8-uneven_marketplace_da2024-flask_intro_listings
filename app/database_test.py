5ed741fff424d04b07790a3c0aed66e8ee03405e
Milan-waleffe <milanwalef@gmail.com>
2024-11-25 12:03:29 +0100

    opkuis

---
+++ app/database_test.py
@@ -0,0 +1,13 @@
+from database import get_db_connection
+import os
+
+# Debug: Print DATABASE_URL
+print("DATABASE_URL:", os.getenv("postgresql://postgres.rfgfcpljgfwblooetbms:[CulinaryHeritage123è]@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"))
+
+try:
+    conn = get_db_connection()
+    print("Database connection successful!")
+    conn.close()
+except Exception as e:
+    print(f"Failed to connect to the database: {e}")
+

ee65ac31657e82800cbdb5ee86237d8ed18d95d0
Annaerme <annaerme.vermeulen@ugent.be>
2024-12-02 16:55:31 +0100

    Nieuw leeg

--- app/config.py
+++
@@ -1,4 +0,0 @@
-class Config: 
-    SECRET_KEY = "a_really_long_and_random_string_1234567890!@#$%^&*()"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.pukfiejqjaztcjjekaty:programmerensuckt1234@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False

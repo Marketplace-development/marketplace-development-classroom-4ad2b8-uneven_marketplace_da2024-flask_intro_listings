c71cdca394465e24407ffbb760f6a132e557ee2f
Clemencevdo <clemencevandenostende@gmail.com>
2024-12-03 11:04:53 +0100

    Wijzigingen in run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
-from app import create_app
+import sys
+from os import path
+
+# Voeg de map 'project' toe aan sys.path
+sys.path.append(path.join(path.dirname(__file__), 'project'))
+
+from app import create_app  # Importeer create_app vanuit de app-module
 
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
-from flask import Flask
-app = Flask(__name__)

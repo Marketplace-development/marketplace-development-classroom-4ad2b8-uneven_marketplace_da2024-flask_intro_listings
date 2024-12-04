5c84deee92ccb96ab55a74eded6f662bacf7e9ad
Milan-waleffe <milanwalef@gmail.com>
2024-12-04 10:50:46 +0100

    aanpassing

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config:
     # Database connection string
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres.rfgfcpljgfwblooetbms:31PaAB6ILljbFjZX@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
+        "postgresql://postgres.rfgfcpljgfwblooetbms:CulinaryHeritage123è@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")

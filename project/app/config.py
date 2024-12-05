9dff7876c54afe326008dad7556d800f327791b5
Astridpnsrt <astrid.pensaert@gmail.com>
2024-12-05 15:58:41 +0100

    test

--- project/app/config.py
+++ project/app/config.py
@@ -2,3 +2,4 @@ class Config:
     SECRET_KEY = "a_really_long_and_random_string_1234567890!@#$%^&*()"
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.pukfiejqjaztcjjekaty:Programmerensuckt1234@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    

eb1053330a41b74999cd7c2d2a40c38053decd8d
vrankenhannelore <hannelore.vranken@gmail.com>
2024-12-11 11:47:13 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, FileField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextAreaField, FileField, DecimalField
+from wtforms.validators import DataRequired, Length, NumberRange
 
 class TitleForm(FlaskForm):
-    title = StringField('Recepttitel', validators=[DataRequired(), Length(max=100)])
-    photo = FileField('Upload een afbeelding (optioneel)')
+    title = StringField('Recipe Title', validators=[DataRequired(), Length(max=100)])
+    photo = FileField('Upload an Image (optional)')
 
 class DescriptionForm(FlaskForm):
-    description = TextAreaField('Korte beschrijving', validators=[DataRequired()])
+    description = TextAreaField('Give a brief description of your recipe', validators=[DataRequired()])
 
 class IngredientsForm(FlaskForm):
-    ingredients = TextAreaField('Ingrediënten', validators=[DataRequired()])
+    ingredients = TextAreaField('List Ingredients here...', validators=[DataRequired()])
 
 class StepsForm(FlaskForm):
-    steps = TextAreaField('Bereidingsstappen', validators=[DataRequired()])
+    steps = TextAreaField('Give detailed steps to prepare the recipe', validators=[DataRequired()])
+
+class PriceForm(FlaskForm):
+    price = DecimalField(
+        'Enter the price for your recipe (in EUR):',
+        validators=[DataRequired(), NumberRange(min=0)],
+        places=2
+    )
